44fb0bcecba8d58d3e3499b5786fd8f749354daf
CharlyMannion <charlottemannion100@gmail.com>
2020-12-15 18:20:57 +0000

    adds endpoints json

--- routes/api.router.js
+++ routes/api.router.js
@@ -3,9 +3,19 @@ const episodesRouter = require('./episodes.router');
 const locationsRouter = require('./locations.router');
 const charactersRouter = require('./characters.router');
 
-
 apiRouter.use('/episodes', episodesRouter);
 apiRouter.use('/locations', locationsRouter);
 apiRouter.use('/characters', charactersRouter);
+
+piRouter.get('/', (req, res) => {
+    res.json({
+        '/api/episodes': ['GET', 'POST'],
+        '/api/episodes/:episode_id': ['GET'],
+        '/api/locations': ['GET', 'POST'],
+        '/api/locations/:location_id': ['GET'],
+        '/api/characters/': ['POST', 'GET'],
+        '/api/characters/:character_id': ['PATCH', 'GET'],
+    })
+})
 
 module.exports = apiRouter;
