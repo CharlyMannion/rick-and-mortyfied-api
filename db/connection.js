5a9498eab060b5eeb69e87e7c58cf3242f7b391b
CharlyMannion <charlottemannion100@gmail.com>
2020-12-15 18:14:53 +0000

    adding start script

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,11 @@
-module.exports = require('knex')(require('../knexfile'));
+// module.exports = require('knex')(require('../knexfile'));
+
+const ENV = process.env.NODE_ENV || 'development';
+
+const knex = require('knex');
+
+const dbConfig =
+    ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } :
+    require('../knexfile');
+
+module.exports = knex(dbConfig);
