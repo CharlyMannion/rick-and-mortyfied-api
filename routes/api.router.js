91a694261529841f206905f1a47cdb845c3af7de
CharlyMannion <charlottemannion100@gmail.com>
2020-12-16 10:46:02 +0000

    unhashes out json

--- routes/api.router.js
+++ routes/api.router.js
@@ -7,15 +7,15 @@ apiRouter.use('/episodes', episodesRouter);
 apiRouter.use('/locations', locationsRouter);
 apiRouter.use('/characters', charactersRouter);
 
-// apiRouter.get('/', (req, res) => {
-//     res.json({
-//         '/api/episodes': ['GET', 'POST'],
-//         '/api/episodes/:episode_id': ['GET'],
-//         '/api/locations': ['GET', 'POST'],
-//         '/api/locations/:location_id': ['GET'],
-//         '/api/characters/': ['POST', 'GET'],
-//         '/api/characters/:character_id': ['PATCH', 'GET'],
-//     })
-// })
+apiRouter.get('/', (req, res) => {
+    res.json({
+        '/api/episodes': ['GET', 'POST'],
+        '/api/episodes/:episode_id': ['GET'],
+        '/api/locations': ['GET', 'POST'],
+        '/api/locations/:location_id': ['GET'],
+        '/api/characters/': ['POST', 'GET'],
+        '/api/characters/:character_id': ['PATCH', 'GET'],
+    })
+})
 
 module.exports = apiRouter;
